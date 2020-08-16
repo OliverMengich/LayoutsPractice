45092919ebdf9303bddcc78c50e0b807a21af8da
OliverMengich <kemeioliver2000@gmail.com>
2020-08-16 20:11:25 +0300

    LayoutsPrac

---
+++ Assignment/Card_Payment.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Assignment
+{
+    public partial class Card_Payment : Page
+    {
+        public Card_Payment()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Assignment/Payment_Type.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Assignment
+{
+    public partial class Payment_Type : Page
+    {
+        public Payment_Type()
+        {
+            InitializeComponent();
+        }
+    }
+}
